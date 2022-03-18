841e420f601a2ca7cfc04ab7b2165e94e32c4bf3
Chris <66043010+poheniks@users.noreply.github.com>
2022-03-17 20:18:45 -0500

    Updated .csproj for correct debuggign & build

---
+++ ScenePhysicsImplementerSubModule.cs
@@ -0,0 +1,17 @@
+﻿
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.Engine;
+using TaleWorlds.InputSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+
+namespace ScenePhysicsImplementer
+{
+    public class ScenePhysicsImplementerSubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            SCEMath.DebugMessage("Scene Physics Implementer Submodule Loaded");
+        }
+    }
+}
